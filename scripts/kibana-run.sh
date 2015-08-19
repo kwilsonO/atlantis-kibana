72ec5b970ded67b598c0b889dfe5dc411e6ce564
root <root@ip-10-181-123-137.ec2.internal>
2015-08-19 21:38:39 +0000

    changed run command

--- scripts/kibana-run.sh
+++ scripts/kibana-run.sh
@@ -1,7 +1,7 @@
 KIBANAVER="kibana-4.1.1-linux-x64"
 REPONAME="atlantis-kibana"
-REPOPATH="/root/elk"
-REPODIR="${REPOPATH}/${REPONAME}"
+REPOPATH="/root/elk/${REPONAME}"
+KIBANADIR="${REPOPATH}/${KIBANAVER}"
 ATLANTISLOGDIR="/var/log/atlantis"
 KIBANALOGDIR="${ATLANTISLOGDIR}/kibana"
 CONFIGDIR="${REPODIR}/config-files"
@@ -16,4 +16,4 @@ if [ -e "${KIBANALOGDIR}/err.log" ]; then
 	rm "${KIBANALOGDIR}/err.log"
 fi
 
-$REPODIR/bin/kibana --config "${CONFIGDIR}/kibana.yaml" > "${KIBANALOGDIR}/out.log" 2> "${KIBANALOGDIR}/err.log" &
+$KIBANADIR/bin/kibana --config "${CONFIGDIR}/kibana.yaml" > "${KIBANALOGDIR}/out.log" 2> "${KIBANALOGDIR}/err.log" &
